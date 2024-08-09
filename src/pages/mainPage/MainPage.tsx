2263a01c152b4d0bf2b1b969ae2f8f707598f7d2
Daria Motorina <lady.renfri@gmail.com>
2024-08-09 16:52:42 +0300

    Добавлен аватар и менюшка с логаутом

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useAppDispatch } from '../../hooks';
 import { logout } from '../login/action';
-import { Button } from '@mantine/core';
+import { Button, Container } from '@mantine/core';
 import { HeaderForm } from './components/headerForm';
 
 export const MainPage = () => {
@@ -10,21 +10,19 @@ export const MainPage = () => {
     return (
         <div>
             <HeaderForm />
-            <h1>I'm main page!</h1>
 
-            <Button 
-                component='a'
-                href="/forms"
-                fullWidth 
-                mt="xl" 
-                size="md"
-                mb={30}
-            >
-                Создать форму
-            </Button> 
-
-            <button 
-                onClick={() => dispatch(logout())}>Log out</button>
+            <Container>
+                <Button 
+                    component='a'
+                    href="/forms"
+                    fullWidth 
+                    mt="xl" 
+                    size="md"
+                    mb={30}
+                >
+                    Создать форму
+                </Button> 
+            </Container>
         </div>
     )
 }
--- src/pages/mainPage/components/headerForm.tsx
+++ src/pages/mainPage/components/headerForm.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
 
-import { Menu, Group, Center, Burger, Container, Drawer, Input, Avatar } from '@mantine/core';
+import { useAppDispatch } from '../../../hooks';
+import { logout } from '../../login/action';
+
+import { Menu, Burger, Container, Drawer, Input, Avatar } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { IconChevronDown } from '@tabler/icons-react';
-import { MantineLogo } from '@mantinex/mantine-logo';
 import { IconSearch } from '@tabler/icons-react';
 
 import '../style.sass'
 
 export const HeaderForm = () => {
+    const dispatch = useAppDispatch()
+
     const [opened, { open, close }] = useDisclosure(false);
-  
+
     return (
       <header className="header">
         <Container size="md">
@@ -20,8 +23,16 @@ export const HeaderForm = () => {
             </Drawer>
             <Burger opened={opened} onClick={open} size="sm" lineSize={2} />
             <Input placeholder="Search" leftSection={<IconSearch size={16} />} />
-            <Avatar name="Kate Kok" color="initials" />
-
+            <Menu shadow="md" width={200}>
+              <Menu.Target>
+                <Avatar name="Kate Kok" color="initials" />
+              </Menu.Target>
+              <Menu.Dropdown>
+                <Menu.Item onClick={() => dispatch(logout())}>
+                  Log out
+                 </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
           </div>
         </Container>
       </header>
